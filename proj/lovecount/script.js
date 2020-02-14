5fe8fde1fb2ebc5594c48813b90d56d7e1e33198
thermitex <thermitex@outlook.com>
2020-02-14 23:21:32 +0800

    Add Lovecount

---
+++ proj/lovecount/script.js
@@ -0,0 +1,11 @@
+function getDaysBetween(dateString1,dateString2){
+	var startDate = Date.parse(dateString1);
+	var endDate = Date.parse(dateString2);
+	var days =(endDate - startDate)/(1*24*60*60*1000);
+	// alert(days);
+	return  days;
+}
+
+window.onload=function(){
+	document.getElementById("count").innerText = getDaysBetween("2017-08-28", new Date());
+}
